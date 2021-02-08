e830577adf6dbf2c58d529cd855fa287602b1c25
Jandelson Farias <jandelson.farias@simplifiquegp.com.br>
2021-02-08 08:26:13 -0300

    implementacao do push notification android

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -6,13 +6,24 @@ import 'AdiantamentoTabs.dart';
 import 'ApontamentoTabs.dart';
 import 'Login.dart';
 import 'PrestacaoContasTabs.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String _message = '';
+
+  _registerOnFirebase() {
+    _firebaseMessaging.subscribeToTopic(usuarioLogado.UsuarioUID);
+    _firebaseMessaging.getToken().then((token) => print(token));
+  }
 
   HelperDB helperDB = HelperDB();
   Usuario usuarioLogado = Usuario();
@@ -20,11 +31,23 @@ class _HomeState extends State<Home> {
   @override
   void initState(){
     super.initState();
-
     helperDB.getUsuarioLogado().then((usuario){
       setState(() {
         usuarioLogado = usuario;
+        _registerOnFirebase();
+        getMessage();
       });
+    });
+  }
+
+  void getMessage() {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('received message ${message["notification"]["body"]}');
+      }, onResume: (Map<String, dynamic> message) async {
+        print('on resume ${message["notification"]["body"]}');
+      }, onLaunch: (Map<String, dynamic> message) async {
+        print('on launch ${message["notification"]["body"]}');
     });
   }
 
